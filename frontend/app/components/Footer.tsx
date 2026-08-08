5279037d7bcb5c183c17af77809955509a7b9cb6
Phuoc <23521228@gm.uit.edu.vn>
2026-08-08 18:09:00 +0700

    feat/Thêm footer, tính năng toggle màu với cả footer

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -1,10 +1,7 @@
 export default function Footer(){
     return(
-        <>
-        
-        
-        
-        </>
+        <footer className="footer">
+  © {new Date().getFullYear()} G-Scores. My assignment for the Full-Stack JavaScript Developer position :3
+        </footer>
     )
-
 }
