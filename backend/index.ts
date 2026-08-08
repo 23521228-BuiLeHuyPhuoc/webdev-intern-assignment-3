81908056ae1e8bb6bdcf8a6de63f6ef460c2adc2
Phuoc <23521228@gm.uit.edu.vn>
2026-08-08 20:49:13 +0700

    /Sửa file index.ts để deploy được lên vercel

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,7 @@
-import express, {type Request, type Response} from "express";
+import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import router from "./routes/index.route";
-import { connectDatabase } from "./config/database";
 const app=express();
 
 app.use(express.json());
@@ -13,11 +12,11 @@ app.use(cors({
     credentials:true
 }));
 app.use("/",router);
-try{
-    await connectDatabase();
-}catch(error){
-    process.exit(1);
+
+if (!process.env.VERCEL) {
+    app.listen(process.env.port || 5000,()=>{
+        console.log(`Server đang chạy ở cổng http://localhost:${process.env.port || 5000}/`);
+    });
 }
-app.listen(process.env.port,()=>{
-    console.log(`Server đang chạy ở cổng http://localhost:${process.env.port}/`);
-})
+
+export default app;
